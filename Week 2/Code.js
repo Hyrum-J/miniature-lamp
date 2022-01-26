c719e023da93598979e17a2fd3e095cf647582c8
Hyrum-J <97466306+Hyrum-J@users.noreply.github.com>
2022-01-26 10:25:36 -0700

    Add files via upload

---
+++ Week 2/Code.js
@@ -0,0 +1,57 @@
+function blastoff() {
+
+    console.log("blastoff() started");
+
+    var i = 50;
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 5000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 10000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 15000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 20000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 25000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 30000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 35000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 40000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 45000);
+
+    setTimeout(function () {
+        document.getElementById("Blastoff Text").innerHTML = i;
+        i = i - 5;
+    }, 50000);
+
+    document.getElementById("Blastoff Text").innerHTML = i;
+};
